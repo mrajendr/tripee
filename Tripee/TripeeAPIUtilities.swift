a8c4f007c5bd0482fbf8a635552836dd4e909486
Ariana Fontenot <arianafontenot@587213269.guest.umich.net>
2015-01-17 01:55:03 -0500

    googleplaces API attempt

---
+++ Tripee/TripeeAPIUtilities.swift
@@ -0,0 +1,18 @@
+//
+//  TripeeAPIUtilities.swift
+//  Tripee
+//
+//  Created by Ariana Fontenot on 1/16/15.
+//  Copyright (c) 2015 Douglas Bumby. All rights reserved.
+//
+
+import Foundation
+
+
+class TripeeAPIUtilities: NSObject {
+    
+    // Get url
+    
+    
+    
+}
--- Tripee/ViewController.swift
+++ Tripee/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let googlePlacesAPIKey: String = "AIzaSyDcJfTNnahsbMXH2gjkj9KIqw4GqzD01_U"
+    private let baseURL  = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key="
+    
+    lazy var data = NSMutableData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Get url
+        let taskURL = NSURL(string: baseURL + googlePlacesAPIKey)
+        var request: NSURLRequest = NSURLRequest(URL: taskURL!)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)!
+        connection.start()
+        
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
+        self.data.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        var err: NSError
+        // throwing an error on the line below (can't figure out where the error message is)
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        println(jsonResult)
+        
     }
 
     override func didReceiveMemoryWarning() {
