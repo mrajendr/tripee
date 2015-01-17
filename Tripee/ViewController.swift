9c530eac3d2761dfc69afa2527b94f5be6e2c49e
Douglas Bumby <douglas.bumby@icloud.com>
2015-01-16 22:54:35 -0500

    Initial commit

---
+++ Tripee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tripee
+//
+//  Created by Douglas Bumby on 2015-01-16.
+//  Copyright (c) 2015 Douglas Bumby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
